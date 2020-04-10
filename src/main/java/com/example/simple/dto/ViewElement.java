6f5ac17878a7c824c4cb9b987c1faf8c4e3f12de
oksana <oksanaov@gmail.com>
2020-04-10 11:48:26 +0300

    created

---
+++ src/main/java/com/example/simple/dto/ViewElement.java
@@ -0,0 +1,14 @@
+package com.example.simple.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class ViewElement {
+    String name;
+    String value;
+}
